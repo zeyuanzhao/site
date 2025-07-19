87d5e2900550365e75bab2a54e729ecc63a1c488
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2025-07-19 00:36:27 -0400

    card with image

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,22 +1,34 @@
-import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Card, CardBody, CardFooter, Image } from "@heroui/react";
 
 export function ProjectCard({
   title = "Project Title",
   description = "Project Description",
   className = "",
+  imageSrc = undefined,
 }: {
   title: string;
   description: string;
   className?: string;
+  imageSrc?: string | undefined;
 }) {
   return (
-    <Card className={className}>
-      <CardHeader>
-        <CardTitle className="text-2xl font-bold">{title}</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <p>{description}</p>
-      </CardContent>
-    </Card>
+    <div className={"relative flex justify-center " + className}>
+      <div className="pointer-events-none absolute -top-5 left-1/2 z-10 w-[110%] -translate-x-1/2">
+        <Image
+          src={imageSrc}
+          alt={title}
+          className="h-auto w-full rounded-2xl border-4 border-white object-cover shadow-xl shadow-white/10"
+        />
+      </div>
+      <Card className="relative border border-white/10 shadow-2xl shadow-white/10">
+        <CardBody className="h-1/3" />
+        <CardFooter className="p-4">
+          <div className="">
+            <p className="mb-2 text-2xl font-bold">{title}</p>
+            <p>{description}</p>
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
