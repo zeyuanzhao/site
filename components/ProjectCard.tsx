14d9ac91c1ce4eb545e9c0344690d505a3ec6c00
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2025-07-19 22:41:29 -0400

    click on card

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -3,7 +3,7 @@ import { AnimatePresence, motion } from "framer-motion";
 import Link from "next/link";
 import { useState } from "react";
 import { FaGithub } from "react-icons/fa6";
-import { HiArrowsPointingOut, HiOutlineGlobeAlt } from "react-icons/hi2";
+import { HiOutlineGlobeAlt } from "react-icons/hi2";
 
 import { CardFooter } from "./ui/card";
 
@@ -52,8 +52,20 @@ export function ProjectCard({
           }
           exit={{ height: 40, opacity: 1 }}
           transition={{ type: "spring", stiffness: 300, damping: 30 }}
-          className="absolute bottom-0 left-0 z-10 w-full"
+          className="absolute bottom-0 left-0 z-10 w-full hover:cursor-pointer"
           style={{ pointerEvents: "none" }}
+          onClick={(e) => {
+            e.stopPropagation();
+            if (
+              currProjectIdx !== undefined &&
+              projectIdx !== undefined &&
+              setCurrProjectIdx
+            ) {
+              setCurrProjectIdx?.(
+                currProjectIdx === projectIdx ? -1 : projectIdx,
+              );
+            }
+          }}
         >
           <CardFooter
             className={`bg-default/80 flex h-full w-full flex-col justify-end p-0 ${
@@ -84,24 +96,6 @@ export function ProjectCard({
                         <HiOutlineGlobeAlt />
                       </Link>
                     )}
-                    <button
-                      className="hover:cursor-pointer"
-                      type="button"
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        if (
-                          currProjectIdx !== undefined &&
-                          projectIdx !== undefined &&
-                          setCurrProjectIdx
-                        ) {
-                          setCurrProjectIdx?.(
-                            currProjectIdx === projectIdx ? -1 : projectIdx,
-                          );
-                        }
-                      }}
-                    >
-                      <HiArrowsPointingOut />
-                    </button>
                   </motion.div>
                 )}
               </AnimatePresence>
@@ -125,68 +119,3 @@ export function ProjectCard({
     </Card>
   );
 }
-
-/*
-return (
-    <div
-      className={`relative flex justify-center transition-transform duration-500 hover:scale-102 ${
-        className
-      }`}
-    >
-      <div className="pointer-events-none absolute -top-5 left-1/2 z-10 w-[110%] -translate-x-1/2">
-        <Image
-          src={imageSrc}
-          alt={title}
-          className="h-auto w-full rounded-2xl border-4 border-white object-cover shadow-xl shadow-white/10"
-        />
-      </div>
-      <Card className="relative border border-white/10 shadow-2xl shadow-white/10">
-        <CardBody className="flex items-end justify-center px-4 pt-[calc(110%*0.53-32px)] pb-4">
-          <div className="">
-            <div className="mb-3 flex flex-row items-center justify-between">
-              <p className="text-2xl font-bold hover:cursor-pointer hover:underline">
-                {title}
-              </p>
-              <div className="flex items-center gap-2">
-                {githubLink && (
-                  <Button
-                    isIconOnly
-                    variant="ghost"
-                    as={Link}
-                    href={githubLink}
-                    target="_blank"
-                  >
-                    <FaGithub />
-                  </Button>
-                )}
-                {websiteLink && (
-                  <Button
-                    isIconOnly
-                    variant="ghost"
-                    as={Link}
-                    href={websiteLink}
-                    target="_blank"
-                  >
-                    <HiOutlineGlobeAlt />
-                  </Button>
-                )}
-                <Button
-                  isIconOnly
-                  variant="ghost"
-                  onPress={() => {
-                    if (currProjectIdx !== undefined && projectIdx !== undefined && setCurrProjectIdx) {
-                      setCurrProjectIdx?.(currProjectIdx === projectIdx ? -1 : projectIdx);
-                    }
-                  }}
-                >
-                  <HiArrowsPointingOut />
-                </Button>
-              </div>
-            </div>
-            <p className="">{description}</p>
-          </div>
-        </CardBody>
-      </Card>
-    </div>
-  );
-*/
